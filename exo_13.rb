d79a04c2358e629ec0fb5b1b5af63c976f993862
momo <mohamed.bouhleel@gmail.com>
2018-12-27 01:38:49 +0100

    13 14

---
+++ exo_13.rb
@@ -0,0 +1,9 @@
+puts "Votre année de naissance svp ??"
+année_de_naissance = gets.to_i
+
+
+i = année_de_naissance
+begin
+   puts(i)
+   i +=1;
+end until i > 2018
---
+++ exo_14.rb
@@ -0,0 +1,7 @@
+
+puts "un nombre au hasard"
+i = gets.to_i
+while (i >= 0)
+	puts i
+	i -= 1
+end
