b6b0e9cd18e4368540e8942a859bdd5172637b4e
Momo <42479802+momobouhlel@users.noreply.github.com>
2019-01-05 23:39:40 +0100

    Update pyramide.rb

--- pyramide.rb
+++ pyramide.rb
@@ -1,13 +1,5 @@
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? PS: max 25"
-nmbr = gets.to_i + 1
-puts "Voici la pyramide :"
-symbole = "#"
-espace = " "
-a = 5
-nmbr.times do |i|
-print espace * a
-print symbole * i
-puts
-a -= 1
-i += 1
-	end
+print "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? PS : 25 PAS PLUS"
+number = gets.chomp.to_i
+puts " Voici la pyramide : "
+n = number
+  1.upto(n).each{|n|puts ("#" * n).rjust(number)}
