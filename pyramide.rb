251a964587be70d8119423a66fdada43eea5864a
Momo <42479802+momobouhlel@users.noreply.github.com>
2019-01-05 23:40:04 +0100

    Update pyramide.rb

--- pyramide.rb
+++ pyramide.rb
@@ -1,5 +1,10 @@
 print "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? PS : 25 PAS PLUS"
+
 number = gets.chomp.to_i
+
 puts " Voici la pyramide : "
+
 n = number
-  1.upto(n).each{|n|puts ("#" * n).rjust(number)}
+
+  
+1.upto(n).each{|n|puts ("#" * n).rjust(number)}
